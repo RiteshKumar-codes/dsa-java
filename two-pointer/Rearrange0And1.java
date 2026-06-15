ee2a18e9ea8ed3437c3769fb7b2232ac7ec29902
RiteshKumar-codes <riteshk7710@gmail.com>
2026-06-15 18:51:54 +0530

    Question: Rearrange 0's and 1's

---
+++ two-pointer/Rearrange0And1.java
@@ -0,0 +1,26 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int left = 0;
+        int right = arr.length-1;
+        
+        
+        while(left<right){
+            if(arr[left]==0){
+                left++;
+            }
+            else if(arr[right]==1){
+                right--;
+            }
+            else if(arr[left] != 0 && arr[right] != 1){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+            
+        }
+       
+    }
+    
+    
+}
